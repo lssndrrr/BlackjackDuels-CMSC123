ac72b7bc0498d46dba991c15f677d137bfd4475e
jrmellpaz <jermellapaz13@gmail.com>
2023-12-10 20:42:22 +0800

    Removed unused file

--- window.cpp
+++
@@ -1,9 +0,0 @@
-#include "game.hpp"
-
-void initWindow(RenderWindow*& window, View& view, VideoMode& video) {
-    video.height = 720;
-    video.width = 1280;
-    window = new RenderWindow(video, "Blackjack Duels!", Style::Default);
-    view = View(FloatRect(0.f, 0.f, 1280.f, 720.f));
-    window->setVerticalSyncEnabled(false);
-}
