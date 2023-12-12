1268e12f956f6f0f4cf5a1ccfebe056ee7667350
Az <arbusinos@up.edu.ph>
2023-12-12 16:36:16 +0800

    added include for vs code users

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 #include "game.hpp"
 #include "menu.hpp"
 
+// for vs code
+#include "abilities.cpp"
+#include "button.cpp"
+#include "deck.cpp"
+#include "game.cpp"
+#include "leaderboard.cpp"
+#include "mainGame.cpp"
+#include "menu.cpp"
+#include "players.cpp"
+#include "textures.cpp"
+#include "updates.cpp"
+
 int main(){
     menu m;
     m.run();
